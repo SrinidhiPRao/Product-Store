23d0ced9179ba3408898c17956bd241af8a4b182
Srinidhi Rao <srinidhi.prao@gmail.com>
2024-10-06 22:58:14 +0530

    Backend Complete.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
 import { connectDatabase } from './db.js';
+import productRoutes from './routes/product.route.js';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.post("/products", (request, response) => {
-    
-})
+app.use(express.json()); // Allows us to accept json data in req.body
 
-app.listen(5000, () => {
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
     connectDatabase();
     console.log("Server started at http://localhost:5000");
 });
